0964f60394a00d334b538cdc9a13a45ce0da2091
piuras-oguras <szymonpiorkowski46@gmail.com>
2024-10-20 21:47:15 +0200

    Zmiana testu get one

--- test/unit/service/test_creature.py
+++ test/unit/service/test_creature.py
@@ -1,5 +1,8 @@
 from pydoc import describe
 
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
 from model.creature import Creature
 from service import creature as code
 
@@ -9,13 +12,18 @@ sample = Creature(name="Psikuta",
                   description = "Ważne!!! bez S",
                   aka = "Psikutas")
 
+sample2 = Creature(name = "Herr Tusk",
+             aka = "fur deutschland",
+             country = "DE",
+             area = "Berlin",
+             description = "Zdrajca polski uważać na niego")
 def test_create():
     resp = code.create(sample)
     assert resp == sample
 
 def test_get_exist():
-    resp = code.get_one("Psikuta")
-    assert resp == sample
+    resp = code.get_one("Herr Tusk")
+    assert resp == sample2
 
 def test_get_missing():
     resp = code.get_one("Arnold Boczek")
